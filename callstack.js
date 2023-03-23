0288f6230234814fccbea5234bc9db8b8657f516
ninizharadze <nijaradze.nini@gmail.com>
2023-03-23 19:14:48 +0400

    promise

--- callstack.js
+++ callstack.js
@@ -10,3 +10,25 @@ setTimeout(()=>{
 
 console.log(2);   
 // პირველი  გამოვა ეს
+
+
+const getperson = () => {
+    const p = new Promise((resolve, reject)=>{
+        setTimeout(() =>{
+            if(Math.random() * 100 > 50){
+                resolve([
+                    {name: "Alex", age: 34},
+                    {name: "John", age: 34},
+                ]);
+            }else{
+                reject(Error("The person couldn't be fetched"));
+            }
+        }, 1000);
+    });
+    return p;
+};
+
+getperson().then(
+    (result) => console.log(result),
+    (error) => console.log(error)
+);
